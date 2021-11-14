fd5befacf62edc0bbc41fcf051cfc27fb0b419be
Seth-Rothschild <seth.j.rothschild@gmail.com>
2021-11-14 06:41:25 -0500

    Add option to replace existing board

--- goban_irl/ui.py
+++ goban_irl/ui.py
@@ -11,6 +11,12 @@ from goban_irl.board import Board
 from goban_irl.utilities import check_bgr_and_bw
 
 
+def _boxify(string):
+    """Takes a string and returns a nice ascii box"""
+    horizontal_line = (len(string) + 4) * "-"
+    return "+{}+\n|  {}  |\n+{}+\n\n".format(horizontal_line, string, horizontal_line)
+
+
 def _print_missing(board, missing):
     human_readable_coordinates = []
     for row, col in missing:
@@ -29,6 +35,30 @@ def _print_welcome_message():
     print(welcome_message)
 
 
+def _print_cornerloader_text(board_metadata):
+    cornerloader_text = ""
+    if board_metadata["loader_type"] == "virtual":
+        cornerloader_text += (
+            "\nSelect TWO opposite corners of the board (e.g. (1, 1) and (19, 19).\n"
+        )
+        cornerloader_text += (
+            "Selected points will appear as red circles once they are clicked.\n"
+        )
+        cornerloader_text += (
+            "Once you've chosen two corners, you can exit the selector with q.\n\n"
+        )
+
+    elif board_metadata["loader_type"] == "physical":
+        cornerloader_text += "\nSelect FOUR corners of the board.\n"
+        cornerloader_text += (
+            "Selected points will appear as red circles once they are clicked.\n"
+        )
+        cornerloader_text += (
+            "Once you've chosen four corners, you can exit the selector with q.\n\n"
+        )
+    print(cornerloader_text)
+
+
 def _print_loaded_boards(boards_metadata):
     loaded_boards_message = ""
     if boards_metadata is None:
@@ -36,20 +66,56 @@ def _print_loaded_boards(boards_metadata):
     else:
         for i, board in enumerate(boards_metadata):
             loaded_boards_message += "Board {}:\n   loader = {},\n    corners = {},\n    boundaries = {}\n\n".format(
-                i, board['loader_type'], board['corners'], board['cutoffs']
+                i, board["loader_type"], board["corners"], board["cutoffs"]
             )
 
 
-def _print_cornerloader_text(board_metadata):
-    if board_metadata["loader_type"] == "virtual":
-        print("Now, select two opposite corners of the board (e.g. (1, 1) and (19, 19)")
-        print("Selected poitns will appear as red circles once they are clicked.")
-        print("Once you've chosen two corners, you can exit the selector with q.")
+def _print_describe_missing(board, missing_stones, board_name):
+    if len(missing_stones) > 0:
+        print("{} is missing stones".format(board_name))
+        if len(missing_stones) > 5:
+            print("    Missing many stones ({})".format(len(missing_stones)))
+        else:
+            for (row, col) in missing_stones:
+                print(
+                    "    Missing {} stone at {} ({})".format(
+                        board.state[row][col],
+                        board._human_readable_alpha((row, col)),
+                        board._human_readable_numeric((row, col)),
+                    )
+                )
 
-    elif board_metadata["loader_type"] == "physical":
-        print("Now, select FOUR corners of the board")
-        print("Selected points will appear as red circles once they are clicked.")
-        print("Once you've chosen four corners, you can exit the selector with q.")
+
+def _prompt_handler(prompt, true_options, false_options):
+    response = input("{}: ".format(prompt))
+    if response in true_options:
+        return True
+    elif response in false_options:
+        return False
+    else:
+        print("Please input either {} or {}".format(true_options[0], false_options[0]))
+        prompt_handler(prompt, true_options, false_options)
+
+
+def _click(board, missing_stone_location):
+    i, j = missing_stone_location
+    screen_position = board.intersections[i][j]
+    topleft = board.corners[0]
+    click_location = [
+        (screen_position[index] + topleft[index]) // 2 for index in range(2)
+    ]
+    pyautogui.moveTo(click_location[0], click_location[1], 0.1, pyautogui.easeOutQuad)
+    pyautogui.click()
+
+
+def _load_board_from_metadata(metadata):
+    return Board(
+        image=ImageLoader(metadata["loader_type"]).snapshot(),
+        corners=metadata["corners"],
+        detection_function=check_bgr_and_bw,
+        cutoffs=metadata["cutoffs"],
+        flip=metadata["flip"],
+    )
 
 
 def load_corners(board_metadata):
@@ -81,11 +147,20 @@ def load_corners(board_metadata):
     return corners
 
 
-def run_app():
+def run_app(verbose=False):
     t0 = time.time()
     _print_welcome_message()
 
-    if os.path.exists("virtual.json"):
+    virtual_exists = os.path.exists("virtual.json")
+    use_virtual = False
+    if virtual_exists:
+        use_virtual = _prompt_handler(
+            "Do you want to use the existing virtual board?",
+            ["y", "Y", "yes", ""],
+            ["n", "N", "no"],
+        )
+
+    if virtual_exists and use_virtual:
         with open("virtual.json") as f:
             virtual_board_metadata = json.load(f)
     else:
@@ -97,7 +172,15 @@ def run_app():
         with open("virtual.json", "w") as f:
             json.dump(virtual_board_metadata, f)
 
-    if os.path.exists("physical.json"):
+    physical_exists = os.path.exists("physical.json")
+    use_physical = False
+    if physical_exists:
+        use_physical = _prompt_handler(
+            "Do you want to use the existing physical board?",
+            ["y", "Y", "yes", ""],
+            ["n", "N", "no"],
+        )
+    if physical_exists and use_physical:
         with open("physical.json") as f:
             physical_board_metadata = json.load(f)
 
@@ -112,86 +195,27 @@ def run_app():
 
     _print_loaded_boards([virtual_board_metadata, physical_board_metadata])
 
-    # v_corners = [(1755, 1608), (2904, 462)]
-    # p_corners = [(1638, 909), (339, 904), (546, 49), (1390, 58)]
-
-    i = 0
     while True:
-        virtual_board = Board(
-            image=ImageLoader(virtual_board_metadata["loader_type"]).snapshot(),
-            corners=virtual_board_metadata["corners"],
-            detection_function=check_bgr_and_bw,
-            cutoffs=virtual_board_metadata["cutoffs"],
-            flip=virtual_board_metadata["flip"],
-        )
-        physical_board = Board(
-            image=ImageLoader(physical_board_metadata["loader_type"]).snapshot(),
-            corners=physical_board_metadata["corners"],
-            detection_function=check_bgr_and_bw,
-            cutoffs=physical_board_metadata["cutoffs"],
-            flip=physical_board_metadata["flip"],
-        )
+        virtual_board = _load_board_from_metadata(virtual_board_metadata)
+        physical_board = _load_board_from_metadata(physical_board_metadata)
 
         missing_stones_on_virtual = virtual_board.compare_to(physical_board)
         missing_stones_on_physical = physical_board.compare_to(virtual_board)
 
-        if len(missing_stones_on_physical) > 0:
-            print("Physical board is missing some stones:")
-            if len(missing_stones_on_physical) > 5:
-                print(
-                    "    Missing many stones ({})".format(
-                        len(missing_stones_on_physical)
-                    )
-                )
-            else:
-                for (row, col) in missing_stones_on_physical:
-                    print(
-                        "    Missing {} stone at {} ({})".format(
-                            virtual_board.state[row][col],
-                            virtual_board._human_readable_alpha((row, col)),
-                            virtual_board._human_readable_numeric((row,col)),
-                        )
-                    )
-        print('')
-        if len(missing_stones_on_virtual) > 0:
-            print("Virtual board is missing some stones:")
-            if len(missing_stones_on_virtual) > 5:
-                print(
-                    "    Missing many stones ({})".format(
-                        len(missing_stones_on_virtual)
-                    )
-                )
-            else:
-                for (row, col) in missing_stones_on_virtual:
-                    print(
-                        "    Missing {} stone at {} ({})".format(
-                            physical_board.state[row][col],
-                            physical_board._human_readable_alpha((row, col)),
-                            physical_board._human_readable_numeric((row,col)),
-                        )
-                    )
+        if verbose:
+            _print_describe_missing(
+                virtual_board, missing_stones_on_physical, "Physical board"
+            )
+            _print_describe_missing(
+                physical_board, missing_stones_on_virtual, "Virtual board"
+            )
 
-        if len(missing_stones_on_virtual) == 1 and len(missing_stones_on_physical)==0:
-            loc_x, loc_y = missing_stones_on_virtual[0]
-            int_x, int_y = virtual_board.intersections[loc_x][loc_y]
-            topleft_x, topleft_y = virtual_board.corners[0]
-            click_loc = ((topleft_x + int_x) // 2, (topleft_y + int_y) // 2)
-            pyautogui.moveTo(click_loc[0], click_loc[1], .25, pyautogui.easeInBounce)
-            pyautogui.click()
-            #pyautogui.click(click_loc[0], click_loc[1])
-        time.sleep(1)
-        i += 1
-        print("iteration {}".format(i))
-
-
-def _boxify(string):
-    """Takes a string and returns a nice ascii box"""
-    horizontal_line = (len(string) + 4) * "-"
-    return "+{}+\n|  {}  |\n+{}+\n\n".format(horizontal_line, string, horizontal_line)
+        if len(missing_stones_on_virtual) == 1 and len(missing_stones_on_physical) == 0:
+            _click(virtual_board, missing_stone_location)
 
 
 def get_corners(img):
-    title = "corners (press q when done)"
+    title = "corners"
     corners = []
 
     def record_corners(event, x, y, flags, param):
@@ -228,7 +252,7 @@ def calibrate(board_metadata):
     return board.calibrate(
         black_stones=black_stones, white_stones=white_stones, empty_spaces=empty_spaces
     )
-    
+
 
 class ImageLoader:
     def __init__(self, board_type):
@@ -252,17 +276,3 @@ class ImageLoader:
 
 if __name__ == "__main__":
     run_app()
-    # import itertools
-    # p_loader = ImageLoader(board_type="physical")
-    # p_corners = load_physical_board(p_loader)
-    # print("p_corners = {}".format(p_corners))
-    # #p_corners = [(349, 916), (549, 63), (1377, 78), (1630, 912)]
-    # #p_corners = [(1536, 925), (304, 934), (546, 390), (1251, 396)]
-
-    # board = Board(p_loader.snapshot(), p_corners, debug=True)
-    # black_stones = [(17, 17), (1, 1), (1, 17), (17, 1)]
-    # white_stones = [(18, 18), (0, 0), (18, 0), (0, 18)]
-    # empty_spaces = [x for x in set(list(itertools.combinations(list(range(19)) + list(range(19)), 2))) if ((x not in white_stones) and (x not in black_stones))]
-    # function, cutoffs = board.calibrate(black_stones=black_stones, white_stones=white_stones, empty_spaces=empty_spaces)
-    # print(function,
-    #      cutoffs)
